92f98420a645c305d0cf9fde6366072e062f4129
Chien-Ming Lin <jianming1481@gmail.com>
2016-05-23 22:32:53 +0800

    commit

--- mbot_strategy/scripts/strategy.py
+++ mbot_strategy/scripts/strategy.py
@@ -8,7 +8,14 @@ if __name__ == "__main__":
     f.close()
     #print f_data
     j = json.loads(f_data)
-    print j['bin_contents']['bin_A']
+    bin=[j['bin_contents']['bin_A'],j['bin_contents']['bin_B'],j['bin_contents']['bin_C'],j['bin_contents']['bin_D'],j['bin_contents']['bin_E'],j['bin_contents']['bin_F']]
+    for x in bin:
+        print x
+    print j['work_order']
+
+
+
+
 
 
 
